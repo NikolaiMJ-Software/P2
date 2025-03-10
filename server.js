60c82fcfc3a032309208febe382ea71914c1e32d
NikolaiMJ-Software <nmje24@student.aau.dk>
2025-03-10 14:29:33 +0100

    Hello world

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files
+// Serve static files (CSS, JS, images, etc.)
 app.use(express.static(__dirname));
+
+// Serve main.html when accessing the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'main.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
+
