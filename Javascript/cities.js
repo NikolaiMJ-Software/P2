1df12dca6f18c973d02cb05d704682abe38819c7
NikolaiMJ-Software <nmje24@student.aau.dk>
2025-03-13 15:05:30 +0100

    Picture buttons yay

--- Javascript/cities.js
+++ Javascript/cities.js
@@ -1,19 +1,26 @@
 document.addEventListener('DOMContentLoaded', async () => {
     try {
-        const response = await fetch('/cities'); // Fetch cities from server
+        const response = await fetch('/cities'); // Fetch cities from the server
         const cities = await response.json();
         const container = document.getElementById('city-buttons'); // Target div
 
         cities.forEach(city => {
-            // Create button element
+            // Create button
             const cityButton = document.createElement('button');
             cityButton.textContent = city.city;
             cityButton.classList.add('city-button');
+
+            // Set the background to image if image exists
+            if (city.image_path) {
+                cityButton.style.backgroundImage = `url('${city.image_path}')`;
+            }
+
+            // Redirect to new page when clicking button
             cityButton.onclick = () => {
                 window.location.href = `../html/city.html?city=${encodeURIComponent(city.city)}`;
             };
 
-            // Append button below the search bar
+            // add city button
             container.appendChild(cityButton);
         });
 
