e0e269443d4b8f4387f5d98e6f3778c80b8205ec
ctrl-alt-bepe <jbp898@gmail.com>
2025-03-14 13:05:43 +0100

    hopefully this doesnt break anything important

--- Javascript/mail_sender.js
+++ Javascript/mail_sender.js
@@ -4,24 +4,45 @@ const nodemailer = require('nodemailer'); // API that allows sending of Gmails
 const transporter = nodemailer.createTransport({
   service: 'gmail', // This only works with Gmail
   auth: {
-    user: 'alenjethegoat@gmail.com', // Gmail that sends the email
-    pass: 'scww kkaq apgx ogvj', // Unique app password from email account
+    user: 'clickoghent@gmail.com', // Gmail that sends the email
+    pass: 'cfzv uket bqei kkkw', // Unique app password for Gmail account
   },
 });
 
 // sent Gmail data
-const mailOptions = {
-  from: 'alenjethegoat@gmail.com', //should always be the same mail
-  to: 'alenje@hotmail.com', //Reciever email
-  subject: 'Hello from Gmail SMTP!', //Header for the mail
-  text: 'This is a test email.', //Text within the mail
+let mailOptions = {
+  from: 'clickoghent@gmail.com', //should always be the same mail
+  to: '', //Reciever email
+  subject: '', //Header for the mail
+  text: '', //Text within the mail
 };
 
-//Send email, and check for errors
-transporter.sendMail(mailOptions, (error, info) => {
-  if (error) {
-    console.log('Error:', error);
-  } else {
-    console.log('Email sent:', info.response);
-  }
-});
+//Send email with custom data
+function send_mail(receiver, subject, text) {
+  //Input custom data into email dataset
+  mailOptions.to = receiver;
+  mailOptions.subject = subject;
+  mailOptions.text = text;
+  //Send email, and check for error
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log('Error:', error);
+    } else {
+      console.log('Email sent:', info.response);
+    }
+  });
+}
+
+//Mail system for item reservations
+function reservation_mails(buyer_email, seller_email, item_id){
+  send_mail(
+    buyer_email,
+    'Reservation af vare på Click&Hent',
+    item_id
+  );
+  send_mail(
+    seller_email,
+    'En af dine varer er reserveret på Click&Hent',
+    item_id
+  );
+}
