decde1668a32c2d0f31e55add5306e4286664e80
Sebastianplaygames <94458257+Sebastianplaygames@users.noreply.github.com>
2025-03-24 13:27:38 +0100

    added another shop

--- Javascript/db.js
+++ Javascript/db.js
@@ -101,7 +101,7 @@ db.serialize(() => {
             });
         db.run(`INSERT INTO shops (shop_name, city_id, img_path, email) VALUES
             ('Måneby', '1', 'Images/Aalborg/Måneby/månebylogo.jpg', 'nikolai456654@gmail.com'),
-            ('jerrys stuff', '1', 'Images/Aalborg/Måneby/månebylogo.jpg', 'nikolai456654@gmail.com')`, (err) => {
+            ('jerrys vare', '1', 'Images/Aalborg/jerrys_vare/jerry_logo.png', 'nikolai456654@gmail.com')`, (err) => {
                 if (err) console.error('Error inserting data:', err.message);
                 else console.log('Shop inserted.');
             });
@@ -113,8 +113,8 @@ db.serialize(() => {
                 'Images/Aalborg/Måneby/Sage_Joracle_Jet_espressomaskine/variant_1/dv_web_D18000128321830.png', '', 'Den er faktisk virkelig grim', 30, 1),
         
             
-            (1, 1, 'Eiffeltårnet', 1, 1000900, 'Du skal selv hente den', 'Images/Aalborg/Måneby/Eiffeltårnet/Eiffel1.jpg', 'Images/Aalborg/Måneby/Eiffeltårnet/Eiffel2.webp',
-            'Images/Aalborg/Måneby/Eiffeltårnet/Eiffel3.jpg', 'Images/Aalborg/Måneby/Eiffeltårnet/Eiffel4.webp', 'Den er virkelig høj, og lavet af franskmænd', 0, NULL),
+            (1, 2, 'Eiffeltårnet', 1, 1000900, 'Du skal selv hente den', 'Images/Aalborg/jerrys_vare/Eiffeltårnet/Eiffel1.jpg', 'Images/Aalborg/jerrys_vare/Eiffeltårnet/Eiffel2.webp',
+            'Images/Aalborg/jerrys_vare/Eiffeltårnet/Eiffel3.jpg', 'Images/Aalborg/jerrys_vare/Eiffeltårnet/Eiffel4.webp', 'Den er virkelig høj, og lavet af franskmænd', 0, NULL),
             
             (1, 1, 'Samsung Galaxy S25 Ultra 5G smartphone (Titanium Black)', 100, 9499, 'Denne Samsung Galaxy S25 Ultra 5G smarphone er fyldt med banebrydende teknologier og AI, hvilket vil løfte din mobil-oplevelse. Den har en 6,9" Dynamic AMOLED 2x-skærm, en Snapdragon Elite 8-processor og et 200MP hovedkamera',
             'Images/Aalborg/Måneby/Samsung_Galaxy_S25_Ultra_5G_smartphone_(Titanium Black)/titanium_black1.png', 'Images/Aalborg/Måneby/Samsung_Galaxy_S25_Ultra_5G_smartphone_(Titanium Black)/titanium_black2.png', 'Images/Aalborg/Måneby/Samsung_Galaxy_S25_Ultra_5G_smartphone_(Titanium Black)/titanium_black3.png',
