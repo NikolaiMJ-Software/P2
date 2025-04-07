7b99b0987efda23868d028b5960e0638ec3b998c
NikolaiMJ-Software <nmje24@student.aau.dk>
2025-04-07 14:55:10 +0200

    working on signup functionality

--- Javascript/signup.js
+++ Javascript/signup.js
@@ -23,3 +23,54 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+
+const check_box = document.getElementById('shop_user');
+const city_select = document.getElementById('city');
+const store_select = document.getElementById('store');
+
+city_select.parentElement.style.display = 'none';
+store_select.parentElement.style.display = 'none';
+
+check_box.addEventListener('change', async () =>{
+    if (check_box.checked){
+        city_select.parentElement.style.display = 'block';
+        await load_cities();
+    } else{
+        city_select.parentElement.style.display = 'none';
+        store_select.parentElement.style.display = 'none';
+    }
+});
+
+city_select.addEventListener('change', async () => {
+    if(city_select.value){
+        store_select.parentElement.style.display = 'block';
+        await load_stores();
+    }else{
+        store_select.parentElement.style.display = 'none';
+    }
+});
+
+async function load_cities(){
+    const res = await fetch('./get_cities');
+    const cities = await res.json();
+
+    city_select.innerHTML = `<option value="">Ingen</option>`;
+    cities.forEach(city => {
+        const option = document.createElement('option');
+        option.value = city.name;
+        city_select.appendChild(option);
+    });
+}
+
+async function load_stores(){
+    const res = await fetch('./get_stores');
+    const stores = await res.json();
+
+    store_select.innerHTML = `<option value="">Ingen</option>`;
+    stores.forEach(store => {
+        const option = document.createElement('option');
+        option.value = store.name;
+        store_select.appendChild(option);
+    });
+}
