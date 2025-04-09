01ecdf60081d1b1cc09f7e185edbfb67b7c6e3be
Mikkelgan <mikkelgan@live.dk>
2025-04-09 15:10:34 +0200

    stalking is more readable

--- Javascript/server.js
+++ Javascript/server.js
@@ -88,7 +88,7 @@ app.get('/searchpage', (req, res) => {
             res.status(404).json({ error: 'City not found' });
             return;
         }
-        console.log(`City requested: ${city}`); 
+        console.log(`\nCity requested: ${city}`); 
         console.log("User:", req.user?.email);
         res.sendFile(path.join(__dirname, '../HTML/searchPage.html'));
     })
@@ -97,7 +97,7 @@ app.get('/searchpage', (req, res) => {
 //path to productpage
 app.get('/productpage', (req, res) => {
     const product = req.query.id; // Get product id from query    
-    console.log(`Product requested: ${product}`); 
+    console.log(`\nProduct requested: ${product}`); 
     console.log("User:", req.user?.email);
     res.sendFile(path.join(__dirname, '../HTML/product_page.html'));
 });
