d46161a4d5f956518e8c15a607ab91c8b9b96a28
lshayes <lhayes24@student.aau.dk>
2025-04-10 13:11:22 +0200

    fix

--- Javascript/cart.js
+++ Javascript/cart.js
@@ -110,6 +110,7 @@ function fill_table() {
             //creates and fills quantity toggle
             let button_element = document.createElement("td");
             let remove_button = document.createElement("BUTTON");
+            button_element.style.color = "black";
             remove_button.setAttribute("class", button_reserve);
 
             //creates and fills product price element
