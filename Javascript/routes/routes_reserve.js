aee1fa8e8a6f31f1ea7775c7687507ee3b85d656
Fr3Chr <166957195+Fzz1n@users.noreply.github.com>
2025-05-20 11:30:22 +0200

    comments

--- Javascript/routes/routes_reserve.js
+++ Javascript/routes/routes_reserve.js
@@ -53,7 +53,7 @@ export function db_get(query, params) {
     });
 }
 
-//Limits emails an IP can send per hour to prevent flooding
+//Limits emails a computer can send per hour to prevent flooding
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour
   max: 5, // max 5 requests per computer
